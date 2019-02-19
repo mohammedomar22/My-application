d513027b6668d2295bec46174ece29d0047ef7d9
mohammedomar22 <midomizoo@gmail.com>
2019-02-19 02:05:26 +0200

    hello world

---
+++ app/src/main/java/com/modernacademy/itg/CameraActivity.java
@@ -0,0 +1,33 @@
+package com.modernacademy.itg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.camerakit.CameraKitView;
+
+public class CameraActivity extends AppCompatActivity {
+    private CameraKitView cameraKitView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        cameraKitView = findViewById(R.id.camera);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraKitView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        cameraKitView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+}
